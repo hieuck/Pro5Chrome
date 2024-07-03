6f86482decf89fbf6b3da582c06f21f3856133ab
Lê Trung Hiếu <hieuck@users.noreply.github.com>
2024-07-03 18:26:31 +0700

    Update Pro5Chrome.py

--- Pro5Chrome.py
+++ Pro5Chrome.py
@@ -13,11 +13,15 @@ PROFILE_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'profile
 CONFIG_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.json')
 URL_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'URL.json')
 
-# --------------------
-# Chrome configuration
-# --------------------
+# --------------------------
+# Start Chrome configuration
+# --------------------------
 
 # Hàm để đọc đường dẫn Chrome từ config
+
+# Đường dẫn Chrome mặc định nếu không có trong config
+default_chrome_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe'
+
 def read_chrome_path():
     if os.path.exists(CONFIG_FILE):
         with open(CONFIG_FILE, 'r') as file:
@@ -25,6 +29,14 @@ def read_chrome_path():
             return config.get('chrome_path', '')  # Trả về đường dẫn Chrome từ config nếu có
     else:
         return ''
+
+# Đọc danh sách đường dẫn Chrome từ config
+if os.path.exists(CONFIG_FILE):
+    with open(CONFIG_FILE, 'r') as file:
+        config = json.load(file)
+        chrome_paths = config.get('chrome_paths', [default_chrome_path])
+else:
+    chrome_paths = [default_chrome_path]
 
 # Hàm để lưu đường dẫn Chrome vào config
 def save_chrome_path(chrome_path):
@@ -75,13 +87,19 @@ def delete_selected_chrome_path():
         chrome_dropdown['values'] = chrome_paths
         chrome_var.set(chrome_paths[0] if chrome_paths else default_chrome_path)
 
-# --------------------
-# Chrome configuration
-# --------------------
+# Hàm để lưu danh sách đường dẫn Chrome vào config
+def save_chrome_paths_to_config():
+    config = {'chrome_paths': chrome_paths}
+    with open(CONFIG_FILE, 'w') as file:
+        json.dump(config, file, indent=4)
 
-# ----------------------------
-# Chrome profile configuration
-# ----------------------------
+# ------------------------
+# End Chrome configuration
+# ------------------------
+
+# ----------------------------------
+# Start Chrome profile configuration
+# ----------------------------------
 
 # Hàm để đọc danh sách profiles từ tệp
 def read_profiles():
@@ -90,6 +108,9 @@ def read_profiles():
             return json.load(file)
     else:
         return []
+
+# Đọc danh sách profiles từ tệp
+profiles = read_profiles()
 
 # Hàm để lưu danh sách profiles vào tệp
 def save_profiles(profiles):
@@ -107,7 +128,7 @@ def open_chrome_and_add_profile():
             profile_dropdown['values'] = profiles
             update_listbox()
         
-        # Lưu đường dẫn Chrome vào danh sách và config
+        # Lưu đường dẫn Chrome vào danh sách vào config
         new_chrome_path = chrome_var.get()
         if new_chrome_path and new_chrome_path not in chrome_paths:
             chrome_paths.append(new_chrome_path)
@@ -118,23 +139,6 @@ def open_chrome_and_add_profile():
         open_chrome(selected_profile)
     else:
         print("Vui lòng chọn hoặc nhập một profile")
-
-# ----------------------------
-# Chrome profile configuration
-# ----------------------------
-
-# Hàm để đọc danh sách URL từ tệp
-def read_urls():
-    if os.path.exists(URL_FILE):
-        with open(URL_FILE, 'r') as file:
-            return json.load(file)
-    else:
-        return []
-
-# Hàm để lưu danh sách URL vào tệp
-def save_urls(urls):
-    with open(URL_FILE, 'w') as file:
-        json.dump(urls, file, indent=4)
 
 # Hàm để mở Chrome với profile được chọn
 def open_chrome(profile):
@@ -153,12 +157,13 @@ def login_google(profile):
     profile_directory = f"--profile-directory=Profile {profile}"
     subprocess.Popen([chrome_path, profile_directory, login_url])
 
-# Hàm để mở URL được chọn trong Chrome
-def open_url(url):
-    chrome_path = chrome_var.get() or read_chrome_path() or default_chrome_path  # Lấy đường dẫn Chrome từ Combobox, nếu không có thì dùng đường dẫn mặc định
-    if 'chrome.exe' not in chrome_path.lower():
-        chrome_path = os.path.join(chrome_path, 'chrome.exe')
-    subprocess.Popen([chrome_path, url])
+# Hàm để đăng nhập Google với profile từ Combobox
+def login_google_from_combobox(event=None):
+    selected_profile = profile_var.get()
+    if selected_profile:
+        login_google(selected_profile)
+    else:
+        print("Vui lòng chọn một profile từ Combobox")
 
 # Hàm để đóng tất cả các tiến trình Chrome
 def close_chrome():
@@ -170,6 +175,51 @@ def close_chrome():
     except Exception as e:
         print(f"Đã xảy ra lỗi khi đóng Chrome: {e}")
 
+# Hàm để mở profile từ Listbox
+def open_profile_from_listbox(event=None):
+    index = profiles_listbox.curselection()
+    if index:
+        selected_profile = profiles_listbox.get(index)
+        open_chrome(selected_profile)
+    else:
+        print("Vui lòng chọn một profile từ danh sách")
+
+# Hàm để đăng nhập Google với profile từ Listbox
+def login_google_from_listbox(event=None):
+    index = profiles_listbox.curselection()
+    if index:
+        selected_profile = profiles_listbox.get(index)
+        login_google(selected_profile)
+    else:
+        print("Vui lòng chọn một profile từ danh sách")
+
+# Hàm để cập nhật Listbox theo thứ tự ABC
+def update_listbox():
+    profiles_listbox.delete(0, tk.END)
+    for profile in sorted(profiles):
+        profiles_listbox.insert(tk.END, profile)
+
+# --------------------------------
+# End Chrome profile configuration
+# --------------------------------
+
+# ---------
+# Start URL
+# ---------
+
+# Hàm để đọc danh sách URL từ tệp
+def read_urls():
+    if os.path.exists(URL_FILE):
+        with open(URL_FILE, 'r') as file:
+            return json.load(file)
+    else:
+        return []
+
+# Hàm để lưu danh sách URL vào tệp
+def save_urls(urls):
+    with open(URL_FILE, 'w') as file:
+        json.dump(urls, file, indent=4)
+
 # Hàm để lưu URL mới vào danh sách và `URL.json`, chỉ lưu khi URL là mới
 def save_url_to_list_and_file(url):
     urls = read_urls()
@@ -180,16 +230,12 @@ def save_url_to_list_and_file(url):
     else:
         print(f"URL '{url}' đã tồn tại trong danh sách.")
 
-# Hàm để xóa danh sách URL và cập nhật giao diện
-def clear_urls_list():
-    save_urls([])
-    update_urls_listbox()
-
-# Hàm để cập nhật Listbox theo thứ tự ABC
-def update_listbox():
-    profiles_listbox.delete(0, tk.END)
-    for profile in sorted(profiles):
-        profiles_listbox.insert(tk.END, profile)
+# Hàm để mở URL được chọn trong Chrome
+def open_url(url):
+    chrome_path = chrome_var.get() or read_chrome_path() or default_chrome_path  # Lấy đường dẫn Chrome từ Combobox, nếu không có thì dùng đường dẫn mặc định
+    if 'chrome.exe' not in chrome_path.lower():
+        chrome_path = os.path.join(chrome_path, 'chrome.exe')
+    subprocess.Popen([chrome_path, url])
 
 # Hàm để cập nhật Listbox URLs
 def update_urls_listbox():
@@ -239,51 +285,10 @@ def handle_enter(event):
     if event.keysym == 'Return':
         open_and_save_url()
 
-# Hàm để đăng nhập Google với profile từ Combobox
-def login_google_from_combobox(event=None):
-    selected_profile = profile_var.get()
-    if selected_profile:
-        login_google(selected_profile)
-    else:
-        print("Vui lòng chọn một profile từ Combobox")
-
-# Hàm để đăng nhập Google với profile từ Listbox
-def login_google_from_listbox(event=None):
-    index = profiles_listbox.curselection()
-    if index:
-        selected_profile = profiles_listbox.get(index)
-        login_google(selected_profile)
-    else:
-        print("Vui lòng chọn một profile từ danh sách")
-
-# Hàm để mở profile từ Listbox
-def open_profile_from_listbox(event=None):
-    index = profiles_listbox.curselection()
-    if index:
-        selected_profile = profiles_listbox.get(index)
-        open_chrome(selected_profile)
-    else:
-        print("Vui lòng chọn một profile từ danh sách")
-
-# Hàm để lưu danh sách đường dẫn Chrome vào config
-def save_chrome_paths_to_config():
-    config = {'chrome_paths': chrome_paths}
-    with open(CONFIG_FILE, 'w') as file:
-        json.dump(config, file, indent=4)
-
-# Đọc danh sách profiles từ tệp
-profiles = read_profiles()
-
-# Đường dẫn Chrome mặc định nếu không có trong config
-default_chrome_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe'
-
-# Đọc danh sách đường dẫn Chrome từ config
-if os.path.exists(CONFIG_FILE):
-    with open(CONFIG_FILE, 'r') as file:
-        config = json.load(file)
-        chrome_paths = config.get('chrome_paths', [default_chrome_path])
-else:
-    chrome_paths = [default_chrome_path]
+# Hàm để xóa danh sách URL và cập nhật giao diện
+def clear_urls_list():
+    save_urls([])
+    update_urls_listbox()
 
 # Tạo cửa sổ chính
 root = tk.Tk()
@@ -364,7 +369,9 @@ login_button_listbox.pack(side=tk.LEFT, padx=5)
 open_all_chrome_button = ttk.Button(listbox_login_frame, text="Mở Toàn Bộ Chrome", command=open_all_chrome_profiles)
 open_all_chrome_button.pack(side=tk.LEFT, padx=5)
 
-# Hàm tương tác profile
+# ---------------------------
+# Start Hàm tương tác profile
+# ---------------------------
 import pygetwindow as gw
 
 def maximize_selected_chrome():
@@ -412,7 +419,9 @@ minimize_button.pack(side=tk.LEFT, padx=5)
 close_button = ttk.Button(resize_frame, text="Đóng", command=close_selected_chrome)
 close_button.pack(side=tk.LEFT, padx=5)
 
-# Kết thúc tương tác với Profile
+# -------------------------
+# End tương tác với Profile
+# -------------------------
 
 # Xử lý sự kiện nhấp đúp vào một profile trong Listbox
 profiles_listbox.bind('<Double-Button-1>', open_profile_from_listbox)
@@ -538,9 +547,13 @@ open_all_profiles_button.pack(side=tk.LEFT, padx=5, pady=10)
 delete_urls_button = ttk.Button(url_buttons_frame, text="Xóa danh sách URLs", command=clear_urls_list)
 delete_urls_button.pack(side=tk.LEFT, padx=5, pady=10)
 
-# ----------------------------------
-# Nút để mở các tệp profiles.json, config.json và URL.json
-# ----------------------------------
+# -------
+# End URL
+# -------
+
+# --------------------------------------------------------------
+# Start Nút để mở các tệp profiles.json, config.json và URL.json
+# --------------------------------------------------------------
 
 def open_profiles_file():
     subprocess.Popen(['notepad.exe', PROFILE_FILE])
@@ -566,6 +579,10 @@ open_profiles_button.pack(side=tk.LEFT, padx=5, pady=10)
 # Nút để mở URL.json
 open_url_button = ttk.Button(open_buttons_frame, text="Mở URL.json", command=open_url_file)
 open_url_button.pack(side=tk.LEFT, padx=5, pady=10)
+
+# ------------------------------------------------------------
+# End Nút để mở các tệp profiles.json, config.json và URL.json
+# ------------------------------------------------------------
 
 # Chạy GUI
 root.mainloop()
