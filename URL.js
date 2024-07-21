aac23f0ad7e8a92ade821a9b3b582c9d3e8b8f51
Lê Trung Hiếu <hieuck@users.noreply.github.com>
2024-07-21 16:03:45 +0700

    move

--- URL.js
+++
@@ -1,5 +0,0 @@
-[
-    "https://myaccount.google.com/security?utm_source=chrome-profile-chooser",
-    "Google.com.vn",
-    "https://accounts.google.com/v3/signin/identifier?continue=https%3A%2F%2Faccounts.google.com%2Fsignin%2Fchrome%2Fsync%2Ffinish%3Fcontinue%3Dhttps%253A%252F%252Fwww.google.com%252F%26est%3DAHl3n5DSBvS87DctDcPz9GYgzA_7m31S_TS0Rix9BJHtBGTpuTtB3XSoODDhW-jlrkADfvQ1h_Al1EYcACKdLXc&ssp=1&flowName=GlifDesktopChromeSync&ddm=0&dsh=S336257859%3A1719965163802694"
-]
