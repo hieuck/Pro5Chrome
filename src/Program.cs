f63b33c3131fcc1c5df61a4ee33db9838371dc56
dungbanemok <dungbanemok@gmail.com>
2025-11-13 10:40:48 +0000

    build: Add project file and entry point

---
+++ src/Program.cs
@@ -0,0 +1,14 @@
+
+using System;
+using System.Windows.Forms;
+
+public static class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new MainForm());
+    }
+}
